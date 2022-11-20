aa20ea7f415117241cc123a624d0e3000ed4c92f
yashpuneet <yash.puneet02@gmail.com>
2022-11-20 12:25:21 -0800

    Added template file reads to chore_card.js

--- source/chore_card/chore_card.js
+++ source/chore_card/chore_card.js
@@ -1,3 +1,15 @@
+//Template files
+
+const chore_card_html = '../chore_card/chore_card.html';
+const chore_card_css = '../chore_card/chore_card.css';
+
+async function readTemplateFile(file)
+{
+  let template = await fetch(file);
+  let templateText = await template.text();
+  return templateText;
+}
+
 // Called once when document.createElement('chore_card') is called, or
 // the element is written into the DOM directly as <chore_card>
 class Chore_card extends HTMLElement{
@@ -6,62 +18,16 @@ class Chore_card extends HTMLElement{
         let shadowDOM = this.attachShadow({mode: 'open'});
         let articleEl = document.createElement('article');
         let styleEl = document.createElement('style');
-        styleEl.textContent = "\
-        .grid-container {\
-        border-style: solid;\
-        border-radius: 20px;\
-        width: 620px;\
-        display: grid;\
-        grid-template-areas:\
-            'item1 item2 item5'\
-            'item1 item3 item5'\
-            'item1 item4 item5';\
-        grid-template-columns: 120px 400px 75px;\
-        grid-template-rows: 60px 60px 60px;\
-        gap: 10px;\
-        background-color: rgb(181, 177, 177);\
-        padding: 10px;\
-        }\
-        .grid-container > div {\
-        background-color: rgb(181, 177, 177);\
-        text-align: left;\
-        padding: 20px 0;\
-        font-size: 20px;\
-        }\
-        .item1 {\
-        grid-area: item1;\
-        display: flex;\
-        justify-content: center;\
-        align-items: center;\
-        }\
-        .item2 {\
-        grid-area: item2;\
-        }\
-        .item3 {\
-        grid-area: item3;\
-        }\
-        .item4 {\
-        grid-area: item4;\
-        }\
-        .item5 {\
-        grid-area: item5;\
-        display: flex;\
-        justify-content: right;\
-        align-items: top;\
-        }\
-        #assignee {\
-        width: 100px;\
-        height: 100px;\
-        border-radius: 50%;\
-        }\
-        #checkbox {\
-        width: 50px;\
-        height: 50px;\
-        }\
-        #label {\
-            background-color: orange;\
-            padding: 2px;\
-        }";
+
+        const assignDesign = () => {
+          readTemplateFile(chore_card_css)
+            .then((cssoutput) => {
+              styleEl.textContent = cssoutput;
+            });
+        }
+
+        assignDesign();
+    
         console.log(styleEl);
         shadowDOM.appendChild(articleEl);
         shadowDOM.appendChild(styleEl); 
@@ -82,62 +48,17 @@ class Chore_card extends HTMLElement{
     set data(data) {
         // If nothing was passed in, return
         if (!data) return;
+
         let articleEl = this.shadowRoot.querySelector('article');
-        //we might change section text to image.
-        /*articleEl.innerHTML = `
-        <div class="grid-container">
-            <div class="item1">
-            <img id='assignee' src="${data.assigneeSrc}"
-                alt="assignee">
-            </div>
-            <div class="item2">
-            <h3 class="name">${data.choreName}</h3>
-            </div>
-            <div class="item3">
-            <h4 class="date">${data.date}</h4>
-            </div>
-            <div class="item4">
-            <h4 class="sections">${data.section}</h4>
-            </div>
-            <div class="item5">
-            <img id='checkbox' src="${data.boxSrc}"
-                alt="checkbox">
-            </div>
-        </div>`;//this should be the html template of article.*/
-        articleEl.innerHTML = "<!DOCTYPE html>\
-        <html lang=\"en\">\
-        <!--This is the template as a referece for making an individual chore card. -->\
-        <!--use grid for arrangment: assignee in first column; chorename, due data, and section in second column;\
-            check box in the third column. May turn the bar into a progression bar-->\
-        <!--template literal will be used to configured each individual chore card.-->\
-        <head>\
-          <link rel=\"stylesheet\" href=\"chore_card.css\">\
-          <script src=\"https://kit.fontawesome.com/cd99b7ce8b.js\" crossorigin=\"anonymous\"></script>\
-        </head>\
-        <body>\
-          <article id=\"card\">\
-            <div class=\"grid-container\">\
-              <div class=\"item1\">\
-                <img id='assignee' src=\"assignee_img.jpg\"\
-                  alt=\"assignee\">\
-                <!--<i class=\"fa-solid fa-user-ninja\" id=\"assignee\"></i>-->\
-              </div>\
-              <div class=\"item2\">\
-                <h3 class=\"name\">Wash the Bedding</h3>\
-              </div>\
-              <div class=\"item3\">\
-                <h4 class=\"date\">11/23/22</h4>\
-              </div>\
-              <div class=\"item4\">\
-                <span id=\"label\">Bedroom</span>\
-              </div>\
-              <div class=\"item5\">\
-                <i class=\"fa-regular fa-square\" id=\"checkbox\"></i>\
-                <!--<i class=\"fa-regular fa-square-check\" id=\"checkbox\"></i>-->\
-              </div>\
-            </div>\
-          </article>\
-        </body>";
+        
+        const assignSkeleton = () => {
+          readTemplateFile(chore_card_html)
+            .then((htmloutput) => {
+              articleEl.innerHTML = htmloutput;
+            });
+        }
+
+        assignSkeleton();
     }
 }
 
